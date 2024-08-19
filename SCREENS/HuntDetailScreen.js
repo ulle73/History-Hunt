1e431b22d1124d89683a51291eed34813ff76a43
ulle73 <rydqvist89@hotmail.com>
2024-08-20 01:04:28 +0200

    fått in valda huntens info i HuntDetailScreen

--- SCREENS/HuntDetailScreen.js
+++ SCREENS/HuntDetailScreen.js
@@ -2,11 +2,11 @@ import React from 'react';
 import { View, Text, Button, StyleSheet } from 'react-native';
 
 const HuntDetailScreen = ({ navigation, route }) => {
-    const { huntId } = route.params;
+    const { hunt } = route.params;
 
     return (
         <View style={styles.container}>
-            <Text>Hunt Details for {huntId}</Text>
+            <Text>Hunt Details for {hunt.id}</Text>
             {/* Display hunt details here */}
             <Button title="Start Hunt" onPress={() => navigation.navigate('HuntMap')} />
         </View>
